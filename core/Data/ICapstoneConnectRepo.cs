5fb7e27d3fe28b119648c629a21c3930d7f862ac
amvh11 <109570935+amvh11@users.noreply.github.com>
2022-08-23 08:56:30 +1200

    Add files via upload

---
+++ core/Data/ICapstoneConnectRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone_Connect.Model;
+
+namespace Capstone_Connect.Data
+{
+    public interface ICapstoneConnectRepo
+    {
+        IEnumerable<Project> GetAllProjects();
+        Project GetProjectByID(int id);
+    }
+}
---
+++ core/Model/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capstone_Connect.Model
+{
+    public class Project
+    {
+        [Key]
+        public int ID { get; set; }
+        public string TeamName { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+        
+    }
+}
