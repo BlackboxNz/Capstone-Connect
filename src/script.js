63fb6f961f74c5fdd8184dd6ae239c0d2cbc7a29
amvh11 <109570935+amvh11@users.noreply.github.com>
2022-09-20 22:37:32 +1200

    Updated js files

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import { Amplify, API, graphqlOperation } from "aws-amplify";
 
 import awsconfig from "./aws-exports";
-
+import { createTodo } from "./graphql/mutations";
 import { DataStore } from '@aws-amplify/datastore';
 import { User } from './models';
 import { Visitor } from './models';
@@ -216,3 +216,21 @@ async function register(){
         console.log('error signing up:', error);
     }
 }
+
+async function createNewTodo() {
+    const todo = {
+      name: "Use AppSync",
+      description: `Realtime and Offline (${new Date().toLocaleString()})`,
+    };
+  
+    return await API.graphql(graphqlOperation(createTodo, { input: todo }));
+  }
+  
+  const MutationButton = document.getElementById("MutationEventButton");
+  const MutationResult = document.getElementById("MutationResult");
+  
+  MutationButton.addEventListener("click", (evt) => {
+    createNewTodo().then((evt) => {
+      MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+    });
+  });
