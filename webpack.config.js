c174066d02b2d169d1041a743a1ed7951f53e8b1
amvh11 <109570935+amvh11@users.noreply.github.com>
2022-10-09 20:24:10 +1300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/script.js',
+  entry: './src/script.mjs',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'build')
