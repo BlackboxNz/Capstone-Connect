665d165571016ec76847b466fb7120a8d42c6b03
amvh11 <109570935+amvh11@users.noreply.github.com>
2022-10-14 09:29:02 +1300

    Multiple updates

---
+++ Controllers/ProjectImageController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_Connect.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class ProjectImageController : Controller
+    {
+        [HttpGet("GetProjectImage/{id}")]
+        public ActionResult GetItemPhoto(string id)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string imgDir = Path.Combine(path, "img/Projects");
+            string fileName1 = Path.Combine(imgDir, id + ".png");
+            string fileName2 = Path.Combine(imgDir, id + ".jpg");
+            string fileName3 = Path.Combine(imgDir, id + ".gif");
+            string respHeader = "";
+            string fileName = "";
+            if (System.IO.File.Exists(fileName1))
+            {
+                respHeader = "image/png";
+                fileName = fileName1;
+            }
+            else if (System.IO.File.Exists(fileName2))
+            {
+                respHeader = "image/jpeg";
+                fileName = fileName2;
+            }
+            else if (System.IO.File.Exists(fileName3))
+            {
+                respHeader = "image/gif";
+                fileName = fileName3;
+            }
+            else
+            {
+                respHeader = "image/png";
+                fileName = Path.Combine(imgDir, "default.png"); ;
+            }
+
+
+            return PhysicalFile(fileName, respHeader);
+        }
+    }
+}
--- Pages/script.js
+++ Pages/script.js
@@ -1,3 +1,96 @@
-async function createProject() {
+async function test() {
     console.log("test");
 }
+projectfilter("all")
+function projectfilter(c) {
+    var x, i;
+    x = document.getElementsByClassName("project-cont");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+
+//Projects
+const projectsContainer = document.getElementById("projects");
+
+// Fetches all projects from api
+const getAllProjects = () => {
+    const fetchPromise = fetch(`http://localhost:5001/webapi/GetAllProjects`,
+        {
+            headers: {
+                "Accept": "application/json",
+            },
+        }
+    ); 
+    const streamPromise = fetchPromise.then((response) => response.json());
+    streamPromise.then((data) => showAllProjects(data));
+}
+
+const showAllProjects = (projects) => {
+    // Loops through each product to create a project card
+    products.map(function (project) {
+      // Fetch the corresponding image
+      fetch(`http://localhost:5001/webapi/ProjectImage/${project.id}`)
+        .then((response) => response.blob())
+        .then((imageBlob) => {
+          // Create the card with a div element
+          const card = document.createElement("div");
+          card.classList = "card col-3";
+          card.id = product.id;
+          // Create an image element
+          const image = document.createElement("img");
+          const imageObjectURL = URL.createObjectURL(imageBlob);
+          image.src = imageObjectURL;
+          image.alt = `Image of ${project.ProjectName}`;
+          image.height = 150;
+          // Create the title
+          const title = document.createElement("h3");
+          title.classList = "line-clamp-1";
+          title.innerHTML = project.ProjectName;
+
+          // Create the description text
+          const projectOverview = document.createElement("p");
+          projectOverview.classList = `line-clamp-3 justify ${project.ID}-desc`;
+          projectOverview.innerHTML = project.ProjectOverview;
+          //button
+          const button = document.createElement("button");
+          button.classList = "btn";
+          button.addEventListener("click", function () {
+            buyItem(project.ID);
+          });
+          button.innerHTML = "View More";
+          // Place all the elements in the card div
+          card.append(image, title, projectOverview, button);
+          card.setAttribute("onclick", `location.href='template.html'`);
+          projectsContainer.append(card);
+        });
+    });
+  };
