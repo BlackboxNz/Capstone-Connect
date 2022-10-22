17d3616c3f4e21a1d6d05acaeeb5105342d9998f
amvh11 <109570935+amvh11@users.noreply.github.com>
2022-10-22 19:53:36 +1300

    Update ICapstoneConnectRepo.cs

--- Data/ICapstoneConnectRepo.cs
+++ Data/ICapstoneConnectRepo.cs
@@ -45,8 +45,7 @@ namespace Capstone_Connect.Data
         public IEnumerable<Comment> GetAllCommentsByID(int projectID);
         public IEnumerable<Comment> GetComments();
         public Comment GetCommentByID(int id);
-        //public Comment GetProjectComment(Project project)
-        //public IEnumerable<Comment> GetProjectComments(Project project)
+
 
         //Tag functions
         public void AddTag(Tag tag);
